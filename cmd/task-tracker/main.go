fa2578db0bf5d93342cd5119deef8d03df4ee57d
Ivan Sevryukov <sevryukovivan1995@mail.ru>
2025-02-21 12:40:36 +0300

    update

--- cmd/task-tracker/main.go
+++ cmd/task-tracker/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"log"
 
 	"github.com/ivansevryukov1995/Task-Tracker/internal"
 )
@@ -15,15 +14,15 @@ func main() {
 	storage := internal.NewStorage(jsonFileName)
 
 	if err := storage.Load(&tasks); err != nil {
-		fmt.Println(errors.Unwrap(err))
-		fmt.Printf("File %v will be automatically created when you add the task with the --add command.\n", jsonFileName)
+		log.Printf("Error load task: %v\nFile %v will be automatically created when you add the task with the --add command.",
+			err, jsonFileName)
 	}
 
 	cmdFlags := internal.NewCmdFlags()
 	cmdFlags.ExecuteCmd(&tasks)
 
 	if err := storage.Save(&tasks); err != nil {
-		fmt.Println(errors.Unwrap(err))
+		log.Fatalf("Error save task: %v", err)
 	}
 
 }
